ea8fc7f469fe63bac6c8851c039d896b14f3213e
Niall Sharma <Niall.k.sharma@gmail.com>
2023-12-15 17:34:05 -0600

    better organiziation

---
+++ StarDataClean.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import requests
+import json
+
+def run():
+    starURL = "DataSources/michelin_my_maps.csv"
+
+    API_URL = "https://api.api-ninjas.com/v1/city?name="
+
+    df = pd.read_csv(starURL)
+    df.drop(columns=['Address', 'PhoneNumber', 'Url',
+                    'WebsiteUrl', 'FacilitiesAndServices', "Longitude", "Latitude"], inplace=True)
+
+    cleaned = df.dropna(axis = 0)
+    cleaned['Name'].astype('str')
+    cleaned['Location'].astype('str')
+    cleaned['Price'].astype('str')
+    cleaned['Cuisine'].astype('str')
+    cleaned['Award'].astype('str')
+
+    cleaned.loc[:,'Award'] = cleaned['Award'].astype(str).str[0]
+
+    cleaned[['City','Country']] = cleaned.Location.str.split(", ", expand=True)
+    cleaned.drop(columns=['Location'], inplace=True)
+    cleaned.drop(columns=['Description'], inplace=True)
+
+    priceLevel = pd.Series([len(x) for x in cleaned['Price']])
+    cleaned.loc[:,'Price Level'] = priceLevel.values
+    cleaned.loc[:,'Price'] = cleaned['Price'].astype(str).str[0]
+
+
+    cleaned = cleaned.drop(cleaned[cleaned['Award'] == 'B'].index)
+    cleaned = cleaned.drop(cleaned[cleaned['Award'] == 'G'].index)
+
+    cleaned = cleaned.drop(cleaned[cleaned['Cuisine'].str.contains(",")].index)
+
+
+    cleaned.to_csv("DataSources/Cleaned.csv", sep="\t", index=False)
--- genomeClean.py
+++ genomeClean.py
@@ -2,25 +2,25 @@ import pandas as pd
 import requests
 import json
 
+def run():
+    df = pd.read_csv("DataSources/geonames-all-cities-with-a-population-1000.csv", sep=";")
+    dt = pd.read_csv("DataSources/Cleaned.csv", on_bad_lines='warn', sep="\t")
 
-df = pd.read_csv("DataSources/geonames-all-cities-with-a-population-1000.csv", sep=";")
-dt = pd.read_csv("DataSources/Cleaned.csv", on_bad_lines='warn', sep="\t")
+    df.drop(columns=['Geoname ID', "ASCII Name", "Alternate Names", "Feature Class", "Feature Code", "Country Code", "Country Code 2", 
+                    "Admin1 Code", "Admin2 Code", "Admin3 Code", "Admin4 Code", "Elevation", "DIgital Elevation Model", "Timezone", "Modification date", "LABEL EN", "Coordinates"], inplace=True)
 
-df.drop(columns=['Geoname ID', "ASCII Name", "Alternate Names", "Feature Class", "Feature Code", "Country Code", "Country Code 2", 
-                 "Admin1 Code", "Admin2 Code", "Admin3 Code", "Admin4 Code", "Elevation", "DIgital Elevation Model", "Timezone", "Modification date", "LABEL EN", "Coordinates"], inplace=True)
+    cleaned = df.dropna(axis = 0)
 
-cleaned = df.dropna(axis = 0)
+    cleaned["Country name EN"].astype("str")
+    dt["Country"].astype("str")
 
-cleaned["Country name EN"].astype("str")
-dt["Country"].astype("str")
+    booleans = cleaned["Country name EN"].isin(dt["Country"])
 
-booleans = cleaned["Country name EN"].isin(dt["Country"])
+    cleaned = cleaned.loc[booleans, :]
 
-cleaned = cleaned.loc[booleans, :]
+    booleans = cleaned["Name"].isin(dt["City"])
 
-booleans = cleaned["Name"].isin(dt["City"])
+    cleaned = cleaned.loc[booleans, :]
 
-cleaned = cleaned.loc[booleans, :]
-
-cleaned.to_csv("DataSources/CleanedGeoData.csv", sep="\t")
+    cleaned.to_csv("DataSources/CleanedGeoData.csv", sep="\t", index=False)
 
