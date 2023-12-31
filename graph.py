3f7422c61e6f021bcf48ade57f8fe1eea6f8b430
Niall Sharma <Niall.k.sharma@gmail.com>
2023-12-30 23:12:20 -0600

    graphs

--- graph.py
+++ graph.py
@@ -6,7 +6,11 @@ df = pd.read_csv("DataSources/FinalData.csv", on_bad_lines='warn',sep=",", encod
 
 data = pd.DataFrame(data = df)
 
-data.plot.scatter(x="City Population", y="Award")
 
+data.plot.scatter(x="Price Level", y="Award", alpha=0.3)
+
+plt.yticks(np.arange(min(data["Award"]), max(data["Award"])+1, 1))
+plt.xticks(np.arange(min(data["Price Level"]), max(data["Price Level"])+1, 1))
+plt.ticklabel_format(style='plain')
 
 plt.show()
